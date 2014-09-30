054e2683074e85c6aeae88345441bd4bc7f471d1
squelch1 <squelch.21+github@gmail.com>
2014-09-30 12:02:29 -0700

    Populating repository with other projects

---
+++ be2465773/hmwk/Dynamic2Darray/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Bradley Evans
+ *
+ * Created on September 30, 2014, 11:23 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ be2465773/hmwk/In Class Notes and Analysis/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Bradley Evans
+ *
+ * Created on September 9, 2014, 11:15 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string> // for string objects
+
+using namespace std;
+
+/*
+ * 
+ */
+
+
+int main() {
+
+    string address;
+    int i;
+    cout << "Enter your address: ";
+    cin >> address;
+    cout << "\n" << address[i] << endl;
+            
+            
+    return 0;
+}
+
