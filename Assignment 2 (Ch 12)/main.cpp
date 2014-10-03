f8dc8243da44bf4e522c1a739aad8837edca0322
squelch1 <squelch.21+github@gmail.com>
2014-10-03 15:18:59 -0700

    Preparing second wave of assignments

---
+++ Assignment 2 (Ch 12)/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Bradley Evans
+ *
+ * Created on October 3, 2014, 3:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ dicecounter/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Bradley Evans
+ *
+ * Created on October 2, 2014, 10:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
